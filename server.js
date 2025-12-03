7c55480c88997cf90d9b17741386c712e69f92cc
Khalique Ahmed Khan <khalique.ahmed3@gmail.com>
2025-12-04 01:51:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import voucherRouter from "./routes/voucher.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +27,7 @@ app.get("/", (req, res) => {
     res.send("Shopify Voucher Public App Running");
 });
 
-app.listen(PORT, "0.0.0.0", () =>
-    console.log("Server running on port " + PORT)
-);
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
+
