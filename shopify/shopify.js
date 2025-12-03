2d3658dae7be62e4435e6df2b42e15ebe0e61843
Khalique Ahmed Khan <khalique.ahmed3@gmail.com>
2025-12-04 02:14:40 +0500

    Update shopify.js

--- shopify/shopify.js
+++ shopify/shopify.js
@@ -1,5 +1,5 @@
 // shopify.js
-import Shopify from '@shopify/shopify-api';
+import { Shopify, ApiVersion, MemorySessionStorage } from '@shopify/shopify-api';
 import pkg from '@shopify/shopify-api/adapters/node';
 const { nodeAdapter } = pkg;
 
@@ -9,10 +9,11 @@ Shopify.Context.initialize({
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(','),
   HOST_NAME: process.env.HOST.replace(/https?:\/\//, ''),
-  API_VERSION: Shopify.ApiVersion.October25, // change to the version you want
+  API_VERSION: ApiVersion.October25, // choose your API version
   IS_EMBEDDED_APP: true,
-  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(), // or your DB storage
+  SESSION_STORAGE: new MemorySessionStorage(),
   CUSTOM_ADAPTER: nodeAdapter(),
 });
 
 export default Shopify;
+
